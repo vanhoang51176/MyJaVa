fc2489ed3edc3022a463316c3b397dd5009b5160
MyJavaCourse <vanhoang51176@yahoo.com>
2019-05-31 15:27:41 +0700

    code java đơn giản

---
+++ btJAVA/src/comment_code/CommentCodeInJava.java
@@ -0,0 +1,19 @@
+package comment_code;
+/**
+ * 
+ * @author USER
+ *	Đây là comment dùng để tạo Java document ở định dạng HTML từ source code Java.
+ *	Đoạn code này sẽ được trình biên dịch bỏ qua khi thực thi.
+ */
+public class CommentCodeInJava {
+
+	public static void main(String[] args) {
+		// System.out.println("Đây là comment 1 dòng. Dòng này sẽ được trình biên dịch bỏ qua khi thực thi");
+		
+		/*
+		 * System.out.println("Đây là comment nhiều dòng. Dòng này sẽ được trình biên dịch bỏ qua khi thực thi");
+		 */
+		System.out.print("Đây là ví dụ về 3 cách comment code trong Java. Dòng này sẽ được trình biên dịch thực thi.");
+	}
+
+}
