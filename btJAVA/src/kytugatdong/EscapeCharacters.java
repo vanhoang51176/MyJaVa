f673ed6f400ce8e277f84ead893c715006a8dff0
MyJavaCourse <vanhoang51176@yahoo.com>
2019-05-31 15:45:51 +0700

    Ký tự gắt  dòng trong JAVA

---
+++ btJAVA/src/kytugatdong/EscapeCharacters.java
@@ -0,0 +1,16 @@
+package kytugatdong;
+
+public class EscapeCharacters {
+
+	public static void main(String[] args) {
+		System.out.println("Tab: " + "Welcome\tto Freetuts!");
+        System.out.println("Backspace: " + "Welcome to\b Freetuts!");
+        System.out.println("New line: " + "Welcome\nto Freetuts!"); 
+        System.out.println("Welcome to\rFreetuts!"); // /r
+        System.out.println("Single quote: " + "Welcome to\'Freetuts!");
+        System.out.println("Double quote: " + "Welcome to\''Freetuts!");
+        System.out.println("Backslash: " + "Welcome to\\Freetuts!");
+
+	}
+
+}
